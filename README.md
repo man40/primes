1896a513c0c2979e1d28f5441278f69c30974ff2
Manol Manolov <mmanolov@kabbage.com>
2020-04-03 16:23:52 -0400

    add license, readme and more

---
+++ README.md
@@ -0,0 +1,15 @@
+# primes
+
+## Overview
+
+This package contains a command that finds prime factorizations 
+of an integer number. It uses cachier to cache the function output for 15 days
+
+## Install
+
+    pip install sympy==1.5.1 cachier==1.4.2
+    pip install -i https://test.pypi.org/simple/ primes
+
+## Usage
+
+    primes 2983749827348653748576
--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from setuptools import setup
 def do_setup():
     with open('requirements.txt') as fp:
         install_requires = fp.read()
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
     setup(
         name='primes',
-        version='0.1',
+        version='0.4',
         description='Find prime factorizations',
         url='https://github.com/man40/primes',
         author='Manol Manolov',
         author_email='man40dev@gmail.com',
         license='MIT',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         packages=['primes'],
         zip_safe=False,
         scripts=['primes/bin/primes'],
